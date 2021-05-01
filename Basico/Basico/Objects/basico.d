eea01786b41f45e2e35fde0fff80e53994c2e3dc
Vinicius Ramon Reis <55250084+viniciusramon@users.noreply.github.com>
2021-04-30 21:24:30 -0300

    Add files via upload

---
+++ Basico/Basico/Objects/basico.d
@@ -0,0 +1,9 @@
+.\objects\basico.o: Basico.c
+.\objects\basico.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C129\TM4C129.h
+.\objects\basico.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C129\TM4C1294NCPDT.h
+.\objects\basico.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cm4.h
+.\objects\basico.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\basico.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmInstr.h
+.\objects\basico.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmFunc.h
+.\objects\basico.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmSimd.h
+.\objects\basico.o: C:\Keil_v5\ARM\PACK\Keil\TM4C_DFP\1.0.0\Device\Include\TM4C129\system_TM4C129.h
